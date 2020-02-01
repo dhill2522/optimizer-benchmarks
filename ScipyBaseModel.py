56d4aac1744480314ca8490e3237e27385a2e9bd
Daniel Hill <dhill2522@gmail.com>
2020-02-01 11:45:46 -0700

    model improvements

--- ScipyBaseModel.py
+++ ScipyBaseModel.py
@@ -3,36 +3,55 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from scipy.integrate import odeint
+from sqlalchemy import create_engine
 
-n = 40
-data = pd.read_csv('./data/ERCOT_load_2019.csv', parse_dates=['HourEnding'])[:n]
-print(data.head())
-print(len(data))
-time = data['HourEnding']
-load = data['WEST']
+# Makes matplotlib happy plotting pandas data arrays
+pd.plotting.register_matplotlib_converters()
 
 def thermal_storage(t, T, x, load, mass_salt, Cp):
     return 3.6e9*(x - load)/(mass_salt*Cp)
 
-def model(x, time, load):
+def model(gen, time, load):
     mass_salt = 6e6 # kg of salt for thermal energy storage
     cost_nuclear = 0.021 # $/KWh
     cost_salt = 10.98    # $/kg
     T0 = 350 #K
     Cp = 1530 # J/kg K, heat capacity of the salt
     dt = 3600 # seconds/hr
-    gen_nuclear = x*1200 # operating nuclear capacity
     T_hist = []
 
-    for i, _ in enumerate(time):
-        step = odeint(thermal_storage, T0, [0, dt], 
-                args=(gen_nuclear[i], load[i], mass_salt, Cp))
+    for i in range(len(time)):
+        step = odeint(thermal_storage, T0, [0, 1], 
+                args=(gen[i], load[i], mass_salt, Cp))
+        print(step)
         T_hist.append(step[1])
         T0 = step[1]
 
-    return cost_salt*mass_salt + np.sum(gen_nuclear*cost_nuclear)
+    return cost_salt*mass_salt + np.sum(gen*cost_nuclear)
+
 
 if __name__ == "__main__":
-    sol = minimize(model, np.ones(len(time))*0.5, args=(time, load))
-    print(sol)
+    # Create the connection to the database
+    con = create_engine('sqlite:///data/ercot_data.db')
 
+    # Create the sql query
+    query = "SELECT * FROM Load WHERE HourEnding BETWEEN date('2019-10-01') AND date('2019-10-02')"
+
+    # Load the data from the database
+    data = pd.read_sql(query, con, parse_dates=['HourEnding'])
+
+    time = data['HourEnding']
+    load = data['ERCOT']
+
+    nuclear_capacity = 54000
+
+    cost = model(np.ones(len(time))*nuclear_capacity, time, load)
+    print('Cost', cost)
+
+    # sol = minimize(model, np.ones(len(time))*nuclear_capacity, args=(time, load))
+    # print(sol)
+
+    # plt.plot(time, load, label='Load')
+    # # plt.plot(time, sol['x']*1200, label='Nuclear')
+    # plt.legend()
+    # plt.show()
