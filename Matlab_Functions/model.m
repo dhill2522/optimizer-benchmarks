54e3d13de9a1f6167845c93738dba1de696da2c5
Zhu <46076110+qinyuz14@users.noreply.github.com>
2020-02-12 11:17:39 -0700

    Modified the ode part.

--- Matlab_Functions/model.m
+++ Matlab_Functions/model.m
@@ -29,7 +29,20 @@ function [cost_total, T_hist] = model(gen, time, load)
     for i = 1:length(time)
        % Get next temperature by integrating difference between 
        % generation and demand 
+       [~, T_next] = ode45(@(t,T)thermal_storage(t, T, gen(i), load(i), mass_salt, Cp), [0,1], T_next);
        
-       
+        if T_next < tes_min_t
+            cost_total = cost_total + cost_blackout*(tes_min_t - T_next);      
+            T_next     = tes_min_t;
+        end
+
+        if T_next > tes_max_t
+            cost_total = cost_total + cost_oversupply*(T_next - tes_max_t);
+            T_next     = tes_max_t;
+        end
+
+        T_hist = [T_hist,T_next];
+
+    cost_total = cost_total + sum(gen * cost_nuclear);
     end
 end
